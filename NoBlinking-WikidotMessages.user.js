7b7fe9de7248b9b0294011e9b50014687ed78ad7
Queerious <scp.queerious@gmail.com>
2025-06-26 15:43:20 -0700

    Add files via upload

---
+++ NoBlinking-WikidotMessages.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         NoBlinking
+// @namespace    http://tampermonkey.net/
+// @version      2025-06-26
+// @description  Remove the blink from wikidot message notifications.
+// @author       Queerious
+// @match       *://*.wikidot.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=wikidot.com
+// @grant        none
+// ==/UserScript==
+
+function GM_addStyle(css) {
+    const style = document.getElementById("GM_addStyleBy8626") || (function() {
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.id = "GM_addStyleBy8626";
+        document.head.appendChild(style);
+        return style;
+    })();
+    const sheet = style.sheet;
+    sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
+}
+
+GM_addStyle(
+    ' @-webkit-keyframes blinker {      ' +
+    '    from { opacity: 1.0; } to { opacity: 1.0; }       ' +
+    ' } '
+);
